2694b4eb1976a06316f131eace98db96a3212eb6
AparnaDarbha <aparnaadarbha@gmail.com>
2020-02-06 21:16:53 +0530

    first commit

---
+++ src/main/java/mavendemo/sortbyName.java
@@ -0,0 +1,8 @@
+package mavendemo;
+import java.util.Comparator;
+public class sortbyName implements Comparator<sweet> {
+      
+	public int compare(sweet g1,sweet g2) {
+		return g1.name.compareTo(g2.name);
+	}
+}
